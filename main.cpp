4f7337d863eea09f0ffe512f9af79630ec733377
Shrey118 <147044448+Shrey118@users.noreply.github.com>
2024-04-24 15:54:38 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <ncurses.h>
+#include "menu.h"
+
+int main() {
+
+    //Initialize ncurses
+    initscr();
+    
+    //Don't buffer input 
+    cbreak(); 
+
+    //Enable input from special keys
+    keypad(stdscr, TRUE);
+
+    //Don't echo input
+    noecho(); 
+
+    //Don't display cursor
+    curs_set(0); 
+
+    //Update screen initially (prevents getch() from clearing the screen)
+    refresh(); 
+
+    //Set getch() to non-blocking mode
+    nodelay(stdscr, TRUE); 
+
+    //Start the game from the main menu
+    startMenu();
+
+    //Clear terminal settings
+    endwin();
+
+    return 0;
+}
