7dca58da7335aee2ab9a208da17a7138fd525b5e
tulsyandivyansh <118306885+tulsyandivyansh@users.noreply.github.com>
2024-04-30 22:40:39 +0800

    Update scoreboard.cpp

--- scoreboard.cpp
+++ scoreboard.cpp
@@ -1,10 +1,10 @@
-#include <algorithm>
+#include <vector>
 #include <fstream>
 #include <iostream>
-#include <string>
-#include <vector>
 #include <ncurses.h>
 #include <cstring>
+#include <algorithm>
+#include <string>
 #include "display.h"
 
 using namespace std;
@@ -12,29 +12,24 @@ using namespace std;
 // stores player name and score //
 struct player_scores 
 {
-    string player_alias;
+
     int player_score;
+    string player_alias;
 };
 
 // input player name and score into score file //
 void file_input(string player_alias, int player_score) 
 {
     player_scores player1;
-    player1.player_alias = player_alias;
     player1.player_score = player_score;
+    player1.player_alias = player_alias;
     ofstream score_file;
 
     score_file.open("scores", ios_base::app);
     if (score_file.is_open()) {
-        score_file << player1.player_alias << " " << player1.player_score << endl;
+        score_file << player1.player_score << " " << player1.player_alias << endl;
         score_file.close();
     }
-}
-
-// check which score is higher //
-bool compare_scores(const player_scores& score1, const player_scores& score2) 
-{
-    return score1.player_score > score2.player_score;
 }
 
 // sort scores in order and return score
@@ -56,6 +51,12 @@ vector<player_scores> sort_file()
     return scores;
 }
 
+// check which score is higher //
+bool compare_scores(const player_scores& score1, const player_scores& score2) 
+{
+    return score1.player_score > score2.player_score;
+}
+
 //function to display and output the top 5 highscores
 int highscores() 
 {
@@ -72,14 +73,15 @@ int highscores()
     int row = 12;
     int choice;
         wattron(score_window, A_BOLD);
+        mvwprintw(score_window, 10, 21, "SCORE        NAME");
         mvwprintw(score_window, 3, 24, "TOP  SCORES");
-        mvwprintw(score_window, 10, 21, "NAME        SCORE");
         wattroff(score_window, A_BOLD);
-
+        int i=0;
         //adding only the top 5 names and scores
-        for (int i = 0; i < 5 && i < sortedScores.size(); i++) {
+        while (i < 5 && i < sortedScores.size()) {
             mvwprintw(score_window, row, 21, sortedScores[i].player_alias.c_str());
             mvwprintw(score_window, row++, 33, to_string(sortedScores[i].player_score).c_str());
+            i+=1;
         }
         // highlighting "BACK TO MENU" option
         wattron(score_window, A_REVERSE);
@@ -87,7 +89,7 @@ int highscores()
         wattroff(score_window, A_REVERSE);
         // switching off highlight option
         wrefresh(score_window);
-        for(;;)
+        while(true)
         {
             // getting user input
             choice=getch();
